4b69b0b1e130dce8591a1b0057bb5151db8d46d5
Bhuvana <bhuvana08972@gmail.com>
2026-03-13 10:51:41 +0530

    Part 2: Addedd Shopping Cart functionality

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import '../styles/ProductCard.css';
+import "../styles/ProductCard.css";
 
-function ProductCard({ product }) {
+function ProductCard({ product, onAddToCart }) {
   return (
-    <div className="product-card">
-      <div className="product-image-container">
-        <img 
-          src={product.image} 
-          alt={product.name} 
-          className="product-image"
-        />
-      </div>
-      <div className="product-info">
-        <h3 className="product-name">{product.name}</h3>
-        <p className="product-description">{product.description}</p>
-        <div className="product-footer">
-          <span className="product-price">${product.price}</span>
-          <span className="product-category">{product.category}</span>
-        </div>
-      </div>
+    <div className="card">
+      <img src={product.image} alt={product.name} />
+      
+      <h3>{product.name}</h3>
+      <p>{product.description}</p>
+      <p>${product.price}</p>
+      <span>{product.category}</span>
+
+      <button 
+      className= "add-to-cart-btn"
+      onClick={() => onAddToCart(product)}>
+        Add to Cart
+      </button>
     </div>
   );
 }
